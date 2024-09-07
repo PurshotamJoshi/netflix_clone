b9fa7fe0e41e34c6574380f172bfa9fd85b41a85
PurshotamJoshi <purshotamjoshi1114@gmail.com>
2024-09-07 13:42:51 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('faq-button').addEventListener('click',() => {
+    const para = document.createElement('p');
+    <p>Something added to my identity</p>
+    para.appendChild(p);
+
+    
+
+})
